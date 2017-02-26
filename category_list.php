9458efbdb770a4ac6108cde65a728a7a6b7ef32d
ankurgyani <arg58@njit.edu>
2017-02-26 16:06:49 -0500

    category list updated

--- category_list.php
+++ category_list.php
@@ -29,7 +29,20 @@ $statement->closeCursor();
             <th>&nbsp;</th>
         </tr>
         
-        <!-- add code for the rest of the table here -->
+       
+        
+	<?php foreach ($categories as $category) : ?>
+            <tr>
+                <td><?php echo $category['categoryName']; ?></td>
+                <td><form action="delete_category.php" method="post">
+                    <input type="hidden" name="categoryID"
+                           value="<?php echo $category['categoryID']; ?>">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+
+    </table>
     
     </table>
 
